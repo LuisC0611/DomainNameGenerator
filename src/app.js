bfcda80341f3e612ef382eb29a90a1c976925d75
LuisC0611 <cruzmendoza6@gmail.com>
2023-02-16 01:20:01 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function domaingenerator() {
+  let pronoun = ["The", "Our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let end = [".com", ".net"];
+  let resultArray = [];
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < end.length; e++) {
+          resultArray.push(`${pronoun[p]}${adj[a]}${noun[n]}${end[e]}`);
+        }
+      }
+    }
+  }
+  for (const element of resultArray) {
+    console.log(element);
+  }
+}
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  domaingenerator();
+
+  // console.log("Hello Rigo from the console!");
 };
+// console.log('{pronoun[p]${adj[a]$noun[n]}.com');
